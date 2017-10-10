2a378fcf13c68589de55e5ba55a991dc5474a6cb
Chris <chris@infrascale.com>
2017-10-09 21:33:40 -0700

    take 3

--- app/migrations/Version20170920045744.php
+++ app/migrations/Version20170920045744.php
@@ -22,6 +22,6 @@ class Version20170920045744 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('CREATE TABLE email_log (id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL, sent_at TIMESTAMP DEFAULT \'current_timestamp()\' NOT NULL COMMENT \'(DC2Type:datetime)\', sender TEXT NOT NULL COLLATE latin1_swedish_ci, recipient TEXT NOT NULL COLLATE latin1_swedish_ci, INDEX sent_at (sent_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE email_log (id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL, sent_at TIMESTAMP DEFAULT \'current_timestamp\' NOT NULL COMMENT \'(DC2Type:datetime)\', sender TEXT NOT NULL COLLATE latin1_swedish_ci, recipient TEXT NOT NULL COLLATE latin1_swedish_ci, INDEX sent_at (sent_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
 }
